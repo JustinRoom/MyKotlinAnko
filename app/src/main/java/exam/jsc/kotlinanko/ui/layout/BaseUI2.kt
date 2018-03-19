641565d72158a1a4b1707c68ade6edd0810cb64b
JustinRoom <1006368252@qq.com>
2018-03-19 19:16:08 +0800

    update

---
+++ app/src/main/java/exam/jsc/kotlinanko/ui/layout/BaseUI2.kt
@@ -0,0 +1,70 @@
+package exam.jsc.kotlinanko.ui.layout
+
+import android.content.Context
+import android.os.Build
+import android.support.design.widget.CoordinatorLayout
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.util.TypedValue
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.LinearLayout
+import exam.jsc.kotlinanko.R
+import org.jetbrains.anko.AnkoComponent
+import org.jetbrains.anko.AnkoContext
+import org.jetbrains.anko.appcompat.v7.navigationIconResource
+import org.jetbrains.anko.find
+
+/**
+ * Created on 2018/3/16.
+ * @author jsc
+ */
+abstract class BaseUI2<in T : AppCompatActivity> : AnkoComponent<T> {
+    //    val ankoLogger = AnkoLogger("BaseUI")
+    lateinit var toolbar: Toolbar
+
+    override fun createView(ui: AnkoContext<T>): View {
+        val root = CoordinatorLayout(ui.ctx)
+        root.fitsSystemWindows = true
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            root.elevation = 0f
+        }
+
+        val layout = LinearLayout(root.context)
+        layout.orientation = LinearLayout.VERTICAL
+        root.addView(layout, CoordinatorLayout.LayoutParams(CoordinatorLayout.LayoutParams.MATCH_PARENT, CoordinatorLayout.LayoutParams.MATCH_PARENT))
+
+        val toolbarLayout = createToolbar(ui)
+        layout.addView(toolbarLayout, LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT))
+        val contentView = createContentView(ui)
+        layout.addView(contentView, LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT))
+        return root
+    }
+
+    private fun createToolbar(ui: AnkoContext<T>): View {
+        val toolbarLayout = LayoutInflater.from(ui.ctx).inflate(R.layout.support_toolbar2, null)
+        toolbar = toolbarLayout.find(R.id.toolbar)
+        toolbar.navigationIconResource = R.drawable.ic_arrow_back_white_24dp
+        toolbar.title = "Title"
+        toolbar.subtitle = "SubTitle"
+        return toolbarLayout
+    }
+
+    fun getActionBarSize(context: Context): Int {
+        val typedValue = TypedValue()
+        val attribute = intArrayOf(android.R.attr.actionBarSize)
+        val array = context.obtainStyledAttributes(typedValue.resourceId, attribute)
+        val value = array.getDimensionPixelSize(0, 0)
+        array.recycle()
+        return value
+    }
+
+    fun <T : AppCompatActivity> setSupportActionBar(title: CharSequence, activity: T, naviListener: View.OnClickListener?, menuListener: Toolbar.OnMenuItemClickListener?) {
+        toolbar.title = title
+        activity.setSupportActionBar(toolbar)
+        toolbar.setNavigationOnClickListener(naviListener)
+        toolbar.setOnMenuItemClickListener(menuListener)
+    }
+
+    abstract fun createContentView(ui: AnkoContext<T>): View
+}
